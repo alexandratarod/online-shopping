3c56f688f0e4aa49ee2b9cf4ea117ce33bb27de6
lia <stegaruflavialiana@yahoo.ro>
2023-05-27 20:33:54 +0300

    AFOMS-1 + AFOMS-5 (incomplete)

--- src/main/java/com/example/onlinestore/controllers/OrdersClientController.java
+++ src/main/java/com/example/onlinestore/controllers/OrdersClientController.java
@@ -1,19 +1,50 @@
 package com.example.onlinestore.controllers;
 
+import com.example.onlinestore.model.Orders;
+import com.example.onlinestore.model.UserData;
+import com.example.onlinestore.servicies.OrdersDatabase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class OrdersClientController {
+public class OrdersClientController implements Initializable {
     @FXML
     private Button HomeButton;
+
+    @FXML
+    private TableView<Orders> MyOrders_TableView;
+
+    @FXML
+    private TableColumn<Orders, String> OrderID_Column;
+
+    @FXML
+    private TableColumn<Orders, String> ProductsID_Column;
+
+    @FXML
+    private TableColumn<Orders, String> ProductsName_Column;
+
+    @FXML
+    private TableColumn<Orders, String > Status_Column;
+
+    @FXML
+    private TableColumn<Orders, String> TotalPrice_Column;
+
 
     private Stage stage;
     private Scene scene;
@@ -30,4 +61,26 @@ public class OrdersClientController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    public void OrdersShowData() {
+        OrderID_Column.setCellValueFactory(new PropertyValueFactory<>("orderId"));
+        //ProductsID_Column.setCellValueFactory(new PropertyValueFactory<>("productsID"));
+        //ProductsName_Column.setCellValueFactory(new PropertyValueFactory<>("productsName"));
+        TotalPrice_Column.setCellValueFactory(new PropertyValueFactory<>("totalPrice"));
+        //Status_Column.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+        // Get the client's orders
+        List<Orders> clientOrders = OrdersDatabase.getOrdersByClientId(UserData.username);
+        ObservableList<Orders> orderslistdata = FXCollections.observableArrayList(clientOrders);
+        MyOrders_TableView.setItems(orderslistdata);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        OrdersShowData();
+    }
+
+
 }
+
--- src/main/java/com/example/onlinestore/servicies/OrdersDatabase.java
+++ src/main/java/com/example/onlinestore/servicies/OrdersDatabase.java
@@ -3,16 +3,22 @@ package com.example.onlinestore.servicies;
 import com.example.onlinestore.model.Orders;
 import com.example.onlinestore.model.User;
 import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.Id;
 import org.dizitart.no2.objects.ObjectRepository;
+import org.dizitart.no2.objects.filters.ObjectFilters;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.onlinestore.servicies.FileSystemService.getPathToFile;
 
 public class OrdersDatabase {
 
     private static ObjectRepository<Orders> orderRepository;
+
+    private String clientID;
 
     private static int nextOrderId = 1;
 
@@ -24,10 +30,34 @@ public class OrdersDatabase {
         orderRepository = database.getRepository(Orders.class);
     }
 
+    /*public static void deleteDatabase() {
+        orderRepository.close();
+        boolean deleted = getPathToFile("Orders.db").toFile().delete();
+        if (deleted) {
+            System.out.println("Database deleted successfully.");
+        } else {
+            System.out.println("Failed to delete the database.");
+        }
+    }*/
+
+
     public static void addOrder(Orders order) {
 
         order.setOrderId(nextOrderId++);
         orderRepository.insert(order);
     }
+
+    public static List<Orders> getOrdersByClientId(String username) {
+        List<Orders> orders = new ArrayList<>();
+        for (Orders order : orderRepository.find(ObjectFilters.eq("ClientID", username))) {
+            orders.add(order);
+        }
+        return orders;
+    }
+
+    public static List<Orders> getAllOrders() {
+        return orderRepository.find().toList();
+    }
+
 
 }
